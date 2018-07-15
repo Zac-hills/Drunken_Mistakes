d84eeb87a4a03183e89db7359d1fcfd15ff4ce21
zac-hills <codinfreakgf@gmail.com>
2018-07-15 00:24:17 -0400

    <Relative Path For Library Loading>

--- DrunkenMistakes/src/Native/WindowInformation.java
+++ DrunkenMistakes/src/Native/WindowInformation.java
@@ -4,7 +4,7 @@ public class WindowInformation {
     //need either JNA or JNI fix
     static
     {
-        System.load("C:\\Users\\zackh\\IdeaProjects\\Drunken_Mistakes\\DrunkenMistakes\\NativeSrc\\WindowInfo.dll");
+        System.load(System.getProperty("user.dir")+"\\DrunkenMistakes\\NativeSrc\\WindowInfo.dll");
     }
 
     static public native int[] GetNativeWindowBounds(int PID);
